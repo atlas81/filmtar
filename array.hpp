990f3a42d7b35ccde188d35b45545663b6723c3b
Zsombor Szabó <zsombor640@gmail.com>
2026-05-02 00:30:59 +0200

    func cmplt

--- array.hpp
+++ array.hpp
@@ -11,14 +11,41 @@ class Array {
 
     public:
 
-    Array();
+    Array(size_t kapacitas) : db(0),kapacitas(kapacitas) {
+        pData = new T[kapacitas];
+    }
 
-    ~Array();
+    ~Array() {
+        delete[] pData;
+    }
 
-    size_t size() const;
-    void resize(size_t newsize);
+    size_t size() const {
+        return db;
+    }
+    void resize(size_t newsize) {
+        T* temp = new T[newsize];
 
-    T& operator[](size_t n);
+        size_t cpy_lim = (newsize < db) ? newsize : db;
+        for (size_t i = 0; i < cpy_lim; ++i) {
+            temp[i] = pData[i];
+        }
+        delete[] pData;
+        kapacitas = newsize;
+        pData = temp;
+
+        if (db > kapacitas) db = kapacitas;
+    }
+
+    T& operator[](size_t n) {
+        if (n >= db || n < 0) throw "array:tulindexeles";
+        return pData[n];
+    }
+
+    const T& operator[](size_t n) const {
+        if (n >= db || n < 0) throw "array:tulindexeles";
+        return pData[n];
+    }
+
 };
 
 #endif
